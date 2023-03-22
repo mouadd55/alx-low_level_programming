88a0f91736258bbe557d79b7ac2dfa6af3c090f6
Mouad Oudrib <mouad_oudrib@outlook.com>
2023-03-22 21:53:40 +0000

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table, starting with 0.
+ */
+void times_table(void)
+{
+	int nbr;
+  int i;
+  int prod;
+
+	for (nbr = 0; nbr <= 9; nbr++)
+	{
+		_putchar('0');
+		for (i = 1; i <= 9; i++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			prod = nbr * i;
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
