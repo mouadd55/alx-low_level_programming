196b186c060636a865745f43928050955d4291af
Mouad Oudrib <mouad_oudrib@outlook.com>
2023-03-24 00:16:56 +0000

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,9 +8,13 @@
 
 void print_square(int size)
 {
-	for (int i = 0; i < size; i++)
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (i = 0; i < size; i++)
 	{
-		for (int j = 0; j < (size); j++)
+		for (j = 0; j < (size); j++)
 			_putchar('#');
 		_putchar('\n');
 	}
