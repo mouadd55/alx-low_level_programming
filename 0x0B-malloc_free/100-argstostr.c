5ea45557ebdfb612afc53802f75e1b0048e5b7b6
mouadd55 <mouad_oudrib@outlook.com>
2023-06-14 14:47:54 -0700

    Add malloc_free bonus part

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all the arguments of a program.
+ * @ac: argument count.
+ * @av: argument vector.
+ *
+ * Return: pointer of an array of char
+*/
+
+char *argstostr(int ac, char **av)
+{
+	char	*str;
+	int     n;
+	int     i;
+	int     j;
+	int     m;
+
+	if (!ac)
+		return (0);
+	for (n = i = 0; i < ac; i++)
+	{
+		if (!av[i])
+			return (0);
+		for (j = 0; av[i][j]; j++)
+			n++;
+		n++;
+	}
+	str = malloc((n + 1) * sizeof(char));
+	if (!str)
+	{
+		free(str);
+		return (0);
+	}
+	for (i = j = m = 0; m < n; j++, m++)
+	{
+		if (av[i][j])
+		{
+			str[m] = '\n';
+			i++;
+			m++;
+			j = 0;
+		}
+		if (m < n - 1)
+			str[m] = av[i][j];
+	}
+	str[m] = 0;
+	return (str);
+}
