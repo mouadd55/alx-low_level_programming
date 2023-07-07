b4dcc27049a324da8b83c5738020b2cd1bcb787b
mouadd55 <mouad_oudrib@outlook.com>
2023-07-07 03:46:00 -0700

    Bit manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,9 +9,10 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit;
-	
+
 	if (index > 63)
 		return (-1);
 	bit = (n >> index) & 1;
 	return (bit);
 }
+
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -13,3 +13,4 @@ int	set_bit(unsigned long int *n, unsigned int index)
 	*n = ((1UL << index) | *n);
 	return (1);
 }
+
