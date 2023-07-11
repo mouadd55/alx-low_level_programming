2dd397430f0de9bbf3837d3be2e8441b7b51da80
mouadd55 <mouad_oudrib@outlook.com>
2023-07-11 08:43:08 +0100

    File I/O

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int		fd;
+	char    *buffer;
+	ssize_t	raed_return_value;
+	ssize_t	write_return_value;
+
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * (letters));
+	if (!buffer)
+		return (0);
+	raed_return_value = read(fd, buffer, letters);
+	write_return_value = write(STDOUT_FILENO, buffer, raed_return_value);
+	close(fd);
+	free(buffer);
+	return (write_return_value);
+}
