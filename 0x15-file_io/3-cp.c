115dcc899fb821edada98bc09ccfc14ab116cf1a
mouadd55 <mouad_oudrib@outlook.com>
2023-07-13 07:56:27 +0100

    C - File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,35 @@
 #include "main.h"
 
 /**
+ * open_files - open files 
+ * @file_from: fd of the file which we will copy from
+ * @file_to: fd of the file which we will copy to
+ * @ac: number of arguments
+ * @av: array of arguments
+ * Return: No return value.
+*/
+void	open_files(int ac, char **av, int *file_from, int *file_to)
+{
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	*file_from = open(av[1], O_RDONLY);
+	if (*file_from < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]),
+		exit(98);
+	}
+	*file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (*file_to < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+}
+
+/**
  * main - copy info from file_from to file_to.
  * @ac: number of arguments
  * @av: array of arguments
@@ -17,23 +46,7 @@ int main(int ac, char **av)
 	char	content[1024];
 
 	l_read = 1024;
-	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	file_from = open(av[1], O_RDONLY);
-	if (file_from < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]),
-		exit(98);
-	}
-	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (file_to < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
+	open_files(ac, av, &file_from, &file_to);
 	while (l_read == 1024)
 	{
 		l_read = read(file_from, content, 1024);
