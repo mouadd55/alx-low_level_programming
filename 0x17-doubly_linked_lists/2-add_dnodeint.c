3b7c651e848160224b1cc1adfb20d5b9aa329106
mouadd55 <mouad_oudrib@outlook.com>
2023-08-17 21:05:49 +0100

    Doubly linked lists: Add task 3

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,8 +9,8 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
-    
-    new = malloc(sizeof(dlistint_t));
+
+	new = malloc(sizeof(dlistint_t));
 	if (!new || !head)
 		return (NULL);
 	new->n = n;
--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,2 +1,35 @@
 #include "lists.h"
 
+/**
+ * add_dnodeint_end - Adds a new node at the end of a doubly linked list
+ * @head: The head of the doubly linked list
+ * @n: The number to place in the new node
+ * Return: The new head of the list
+*/
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+	dlistint_t *tmp;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->prev = NULL;
+	if (!*head || !head)
+	{
+		new->next = *head;
+		*head = new;
+		return (*head);
+	}
+	else
+	{
+		tmp = *head;
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = new;
+		new->prev = tmp;
+		new->next= NULL;
+		return (new);
+	}
+}
